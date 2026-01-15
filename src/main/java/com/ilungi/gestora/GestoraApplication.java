3a4d599bd6ddf2102025fcefacac30da162c9a01
Lucian Alfred <lucianalfred60@gmailcom>
2026-01-16 00:19:03 +0100

    init

---
+++ src/main/java/com/ilungi/gestora/GestoraApplication.java
@@ -0,0 +1,13 @@
+package com.ilungi.gestora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoraApplication.class, args);
+	}
+
+}
