193cde4f15e85fc524962cabe178dad7189ae0be
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-19 10:55:54 +0100

    feat: resource- task

--- src/main/java/com/ilungi/gestora/resources/TaskResource.java
+++ src/main/java/com/ilungi/gestora/resources/TaskResource.java
@@ -1,5 +1,28 @@
 package com.ilungi.gestora.resources;
 
-public class TaskResource {
 
+import com.ilungi.gestora.entities.Role;
+import com.ilungi.gestora.entities.Task;
+import com.ilungi.gestora.entities.User;
+
+import java.util.Date;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/tasks")
+public class TaskResource {
+	@GetMapping
+	public ResponseEntity<Task> findAll(){
+		//public Task(Long id, String title, String description, Date createAt, Date endDate, User responsible)
+		User l = new User(1L, "la@gmail.com", "99999","Luciano", "123", Role.USER);
+		Task t =  new Task(1L, "Beber cafe", "Ir a copa beber cafe", new Date(2026, 1, 20), new Date(2026, 2, 20), l);
+		return ResponseEntity.ok().body(t);
+	}
+	
+	
 }
